f83a64f479b6b1d289405837e20cbed270a047d0
SvetlanaReutskaya <lana.reutska@gmail.com>
2020-09-25 19:24:57 +0300

    feat: add solving promlem transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let newArray = [];
+  if (!Array.isArray(arr)) throw new Error();
+
+  newArray = arr.slice();
+  
+  for(let i = 0; i < newArray.length; i ++){
+    
+    switch (newArray[i]) {
+      case '--discard-next': 
+        if (i + 1 < newArray.length) {
+          newArray.splice(i, 2, '-', '-');
+        } 
+        else 
+          newArray[i] = '-';
+        break;
+      
+      case '--discard-prev': 
+        if (i - 1 > 0) {
+          newArray.splice(i - 1, 2, '-', '-');
+        } else  
+          newArray[i] = '-';
+        break;
+      
+      case '--double-next':
+        if (i + 1 < newArray.length && newArray[i + 1] !== '-') {
+          newArray.splice(i, 1, arr[i + 1]);
+        } 
+        else
+          newArray[i] = '-';
+        break;
+
+      case '--double-prev': 
+        if (i - 1 > 0 && newArray[i - 1] !== '-') {
+          newArray.splice(i, 1, arr[i - 1]);
+        } 
+        else 
+          newArray[i] = '-';
+        break;
+      
+    }
+    }
+  return newArray.filter(word => word !== '-');
 };

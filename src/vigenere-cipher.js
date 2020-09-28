8cd09197d3d706c161fc90666a7bfcef186b3f12
SvetlanaReutskaya <lana.reutska@gmail.com>
2020-09-28 15:34:54 +0300

    feat: add solving vizenere-ciper

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,61 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(param) {
+    this.isReverse = param;
+  }
+
+  encrypt(message, key) {
+   let result = '';
+   message = message.toUpperCase();
+   key = key.toUpperCase();
+
+   let j = 0;
+
+   for (let i = 0; i < message.length; i++){
+    if (message[i] >= "A" && message[i] <= "Z") {
+      if(j === key.length) j = 0;
+
+      let code = message[i].charCodeAt();
+      let keyCode = key[j].charCodeAt();
+
+      let resCode = code + keyCode - 65;
+      if (resCode > 90) resCode -=26;
+      
+      result += String.fromCharCode(resCode);
+      j++;
+    }
+    else result += message[i];
+   }
+
+   return this.isReverse ? result.split('').reverse().join('') : result;
+  }   
+
+  decrypt(message, key) {
+    let result = '';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+ 
+    let j = 0;
+ 
+    for (let i = 0; i < message.length; i++){
+     if (message[i] >= "A" && message[i] <= "Z") {
+       if(j === key.length) j = 0;
+ 
+       let code = message[i].charCodeAt();
+       let keyCode = key[j].charCodeAt();
+ 
+       let resCode = code - keyCode + 65;
+       if (resCode < 65) resCode +=26;
+       
+       result += String.fromCharCode(resCode);
+       j++;
+     }
+     else result += message[i];
+    }
+ 
+    return this.isReverse ? result.split('').reverse().join('') : result;
   }
 }
 

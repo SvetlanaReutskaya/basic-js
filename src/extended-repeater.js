9a365d09d2359c56549f9426a87caba7f863eb94
SvetlanaReutskaya <lana.reutska@gmail.com>
2020-09-28 14:05:18 +0300

    feat: add solving extended repeater problem

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,60 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let string = str === null ? 'null' : str.toString();
+  if(options.addition === null) options.addition = 'null';
+  
+  let result = ''
+  
+  if(options.repeatTimes != undefined){
+    for(let i = 0; i < options.repeatTimes - 1; i ++){
+      result += string;
+
+      if(options.addition != undefined){
+        if(options.additionRepeatTimes != undefined){
+          for(let j = 0; j < options.additionRepeatTimes-1; j ++){
+            result += options.addition.toString();
+
+            if(options.additionSeparator != undefined){
+              result += options.additionSeparator;
+            } else {
+              result += '|';
+            }
+          }
+          result += options.addition.toString();
+        } else {
+          result += options.addition.toString();
+        }
+      }
+
+      if(options.separator  != undefined){
+        result += options.separator ;
+      } else {
+        result += '+';
+      }
+    }
+    
+  }
+
+    result += string;
+
+      if(options.addition != undefined){
+        if(options.additionRepeatTimes != undefined){
+          for(let j = 0; j < options.additionRepeatTimes-1; j ++){
+            result += options.addition.toString();
+
+            if(options.additionSeparator != undefined){
+              result += options.additionSeparator;
+            } else {
+              result += '|';
+            }
+          }
+          result += options.addition.toString();
+        } else {
+          result += options.addition.toString();
+        }
+      }
+
+  return result;
 };
   
